47a60d0d3dde69ced2328140accf8253d0705156
Bruce <brucepark0@gmail.com>
2016-09-19 22:21:25 -0700

    Step 6 - Add custom tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  use_doorkeeper
+  use_doorkeeper do
+    controllers :tokens => 'custom_tokens'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   # added for minimal security around adding trusted client apps for oauth
