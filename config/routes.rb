b5170f08485d2d3ed79d4e7793c21ee6829b073d
Bruce <brucepark0@gmail.com>
2016-09-19 22:08:46 -0700

    Protect /oauth/applications endpoint
    - Client apps can register with a truster user name and password

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   use_doorkeeper
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  # added for minimal security around adding trusted client apps for oauth
+  get '/access_oauth_client_apps', to: 'doorkeeper_client_authorization_#access_oauth_client_apps'
 end
