bc7f700f0f9f51ba2c0d4333f15dda1ce68ccd8c
Bruce <brucepark0@gmail.com>
2016-09-19 22:01:09 -0700

    Add resource_authenticator logic for doorkeeper

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -4,10 +4,15 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
+    # fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # Put your resource owner authentication logic here.
     # Example implementation:
     #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    if user_signed_in?
+      current_user
+    else
+      redirect_to root_path
+    end
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
