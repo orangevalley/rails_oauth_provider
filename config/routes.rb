d11dd687a1dcfb799acf8904d913a058d2eac40d
Bruce <brucepark0@gmail.com>
2016-10-08 13:48:07 -0700

    Add static controller and dummy user in seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # added for minimal security around adding trusted client apps for oauth
   get '/access_oauth_client_apps', to: 'doorkeeper_client_authorization_#access_oauth_client_apps'
 
+  root to: 'static#index'
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:user] do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(first_name: "John", last_name: "Johnson", email: "johnjohnson@gmail.com", password: "password", password_confirmation: "password", company: "Ford Motor Co.")
+user.save!
